fa91bfb87557912f18ceee00cf04ef2c8d4f8ecb
Danlei Mou <dmou@student.unimelb.edu.au>
2020-09-06 13:50:57 +0800

    set up database

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
+require('dotenv').config();
+
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const connection = require('./database');
+
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
     res.send('Hello FoliHub!');
-})
+});
+
+
 
 app.listen(process.env.PORT || 3000, () => {
     console.log('Folihub is listening on port ' + PORT)
