1711f375e41eeee10a755ba5be73a1c35485b5df
Danlei Mou <dmou@student.unimelb.edu.au>
2020-09-13 02:55:31 +0800

    try another api fetch method

--- folihub/src/api/userAPI.js
+++ folihub/src/api/userAPI.js
@@ -4,9 +4,26 @@ const axios = require('axios');
 
 export async function postUserSignup(data) {
     const endpoint = BASE_URL + '/signup';
+    /*
     console.log("post user sign up");
     await axios.post(endpoint, data, {withCredentials:true, crossdomain:true}).then(response => {
         console.log("API: "+response);
         return response.json();
     });
+    */
+   console.log("post user sign up:")
+   const response = await fetch(endpoint, {
+        method: 'POST',
+        mode: 'cors',
+        cache: 'no-cache',
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        redirect: 'follow',
+        referrerPolicy: 'no-referrer',
+        body: JSON.stringify(data)
+    });
+    console.log(response);
+    return response.json();
 }
