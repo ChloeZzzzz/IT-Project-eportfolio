5a74d79992ce6a46360a8b66684c59d778fd84a9
Danlei Mou <dmou@student.unimelb.edu.au>
2020-09-14 22:28:28 +0800

    fix contact us

--- folihub/src/components/ContactUs.js
+++ folihub/src/components/ContactUs.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import Welcome from "react-welcome-page";
-import Logo_Light from "../img/folihub_light.png";
-import { FadeDiv, colorPlan,ContactUsText, HomePage, IntroImg, HomeRedirect, FirstIntro, IconComponent, Poster, Poster1, Poster2, Poster3, SecondIntro, AboutProduct, HomepageLogo, HomeTitle, Slogan, HomeComponents, LoginRedirect, SignUpRedirect, SignupText, LoginText, SignUpGoogleRedirect, GoogleIcon, GoogleLink, AboutProductBlock1, AboutProductBlock2, AboutProductImg, AboutProductText  } from "./Style";
-import Google_Icon from "../img/google-icon.png";
-import Lock from "../img/lock.jpg";
+import { colorPlan, ContactUsText } from "./Style";
 
 class ContactUs extends React.Component {
     constructor(props) {
@@ -31,15 +28,13 @@ class ContactUs extends React.Component {
             );
         } else {
             return (
-                <ContactUs>
-                        <h1>Contact Us</h1>
-                        <hr/>
-                        <ContactUsText>
-                        We are undergraduate students of University of Melbourne, Folihub is our graduation project. This Website is developed by TEAM 11 of IT porject(COMP 30023).If you have any questions or concerns about our website,please e-mail to yunweiw@student.unimelb.edu.au. Thank you for your support! 
-                        </ContactUsText>    
-                       
-                    
-                </ContactUs>
+                <div>
+                    <h1>Contact Us</h1>
+                    <hr/>
+                    <ContactUsText>
+                    We are undergraduate students of University of Melbourne, Folihub is our graduation project. This Website is developed by TEAM 11 of IT porject(COMP 30023).If you have any questions or concerns about our website,please e-mail to yunweiw@student.unimelb.edu.au. Thank you for your support! 
+                    </ContactUsText>    
+                </div>
             )
         }
     }
