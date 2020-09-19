fe68f0ff6fbeb8ee6b047b1002f0ca263b63b422
Yifei Zhu <40694262+ChloeZzzzz@users.noreply.github.com>
2020-09-20 01:35:14 +0800

    testing 200 for login

---
+++ test/user.authentication.spec.js
@@ -0,0 +1,31 @@
+//Add promise support if this does not exist natively
+if (!global.Promise) {
+    global.Promise = require('q');
+}
+var chai = require('chai');
+var chaiHttp = require('chai-http');
+const expect = chai.expect;
+
+var userController = require('../controllers/userController');
+
+var app = 'https://folihub-api.herokuapp.com/user';
+
+chai.use(chaiHttp);
+
+describe("GET /login", function (done) {
+    it ("should login with correct email as password", function (done) {
+        this.timeout(3000);
+        let userInfo = {};
+        userInfo.email = "test@test.test";
+        userInfo.password = "test";
+
+        chai.request(app)
+            .get('/login')
+            .send(userInfo)
+            .end(function(err, response) {
+                console.log(response);
+                expect(response).to.has.status(200);
+                done();
+            });
+    });
+});
