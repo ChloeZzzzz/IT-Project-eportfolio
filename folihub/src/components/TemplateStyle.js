fd8dea8f93b67d162ac019318b75b776d8aa3040
Yifei Zhu <40694262+ChloeZzzzz@users.noreply.github.com>
2020-09-27 21:02:55 +0800

    initiate template 1

---
+++ folihub/src/components/TemplateStyle.js
@@ -0,0 +1,42 @@
+import styled from "styled-components";
+
+export const Container_1 = styled.div
+`
+    display: grid;
+    grid-template-rows: 50% 50%;
+    grid-template-columns: 30% 30% 30%;
+    grid-template-areas:
+    "IMG_1 TXT_1",
+    "IMG_2 TXT_2",
+    "IMG_3 TXT_3";
+`;
+
+export const IMG_1 = styled.div
+`
+
+`;
+
+export const TXT_1 = styled.div
+`
+
+`;
+
+export const IMG_2 = styled.div
+`
+
+`;
+
+export const TXT_2 = styled.div
+`
+
+`;
+
+export const IMG_3 = styled.div
+`
+
+`;
+
+export const TXT_3 = styled.div
+`
+
+`;
---
+++ folihub/src/components/template_1.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {Container_1, IMG_1, TXT_1, IMG_2, TXT_2, IMG_3, TXT_3} from '../components/TemplateStyle';
+class Template_1 extends React.Component {
+    render() {
+        return (
+            <Container_1>
+                <IMG_1>
+                </IMG_1>
+                <TXT_1>
+                </TXT_1>
+                <IMG_2>
+                </IMG_2>
+                <TXT_2>
+                </TXT_2>
+                <IMG_3>
+                </IMG_3>
+                <TXT_3>
+                </TXT_3>
+            </Container_1>
+        );
+    }
+}
+export default Template_1;
