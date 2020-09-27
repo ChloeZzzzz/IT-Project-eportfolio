32a0f22b293892c139df9ba44ae48696655fe320
Danlei Mou <dmou@student.unimelb.edu.au>
2020-09-27 21:43:15 +0800

    fixing create eportfolio

--- controllers/ePortfolioController.js
+++ controllers/ePortfolioController.js
@@ -1,7 +1,6 @@
 const db = require('../config/database.js');
 
 const createEPortfolio = async (req, res) => {
-    console.log(req);
     console.log(req.body)
     let {email, layout} = req.body
     try{
@@ -12,22 +11,12 @@ const createEPortfolio = async (req, res) => {
             if (err) {
                 console.log("---creat EP ERROR---");
                 console.log(err);
-                return;
+                return res.status(200).send({"message": 'failed to create EP'});
             }
             console.log("---RESULT---");
             console.log(result);
-
-        });
-        await db.query(`SELECT FolioID, Email FROM Eportfolios WHERE Email = "${email}"`, async function(err, result) {
-            if (err) {
-                console.log("---verify EP creation ERROR---");
-                console.log(err);
-                return;
-            }
-            console.log(result[0].FolioID);
-            console.log(result[0].Email);
-            res.message('create EP success');
-
+            res.status(200).send({"message": 'create EP success'});
+            return res.end();
         });
 
 
