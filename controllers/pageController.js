efa64bf22051c6fc91a96a8a2ba9b31ca482aa75
haoyangyu-aiden <69461885+haoyangyu-aiden@users.noreply.github.com>
2020-10-03 19:54:59 +1000

    add page

--- controllers/pageController.js
+++ controllers/pageController.js
@@ -1,4 +1,55 @@
+/*
+ * @Author: your name
+ * @Date: 2020-10-03 19:53:37
+ * @LastEditTime: 2020-10-03 19:54:41
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \IT-Project-eportfolio\controllers\pageController.js
+ */
 const db = require('../config/database.js');
-const DATE_FORMATER = require('dateformat');
 
-module.exports = {}
+
+const addPage = async (req,res) => {
+    console.log(req.body)
+    let{email, folioId,templateId} = req.body
+    try{
+        console.log(email);
+        console.log(folioId);
+        console.log(templateId);
+        await db.query(`INSERT INTO Pages (FolioID, TemplateID) VALUES ("${folioId}", "${templateId}")`,async function(err, result){
+            if (err) {
+                console.log("---add Page ERROR---");
+                console.log(err);
+                return;
+            }
+            res.message('add Page success');
+            
+        });
+
+
+    }
+    catch(err){
+        console.log("---add Page ERROR---");
+        console.log(err);
+        return;
+    }
+
+    return;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = {
+    addPage,
+    deletePage
+}
--- routes/pageRouter.js
+++ routes/pageRouter.js
@@ -1,3 +1,18 @@
 const express = require('express');
 const pageRouter = express.Router();
 const pageController = require('../controllers/pageController');
+
+
+
+//add a page
+pageRouter.post('/addPage', pageController.addPage);
+
+//delete a page
+pageRouter.post('/deletePage', pageController.deletePage);
+
+
+
+
+
+
+module.exports = pageRouter;
