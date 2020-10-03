6cc59696b81576810fe68d5b570c95a6726a78b1
ruijiepan0319 <pendog319@gmail.com>
2020-10-03 20:01:43 +1000

    deletePage method

--- controllers/pageController.js
+++ controllers/pageController.js
@@ -1,4 +1,14 @@
 const db = require('../config/database.js');
 const DATE_FORMATER = require('dateformat');
 
+const deletePage = async(req,res) => {
+    console.log(req.body)
+    let {pageId, folioId} = req.body
+    try{
+        console.log(pageId);
+        console.log(folioId);
+
+    }
+
+}
 module.exports = {}
