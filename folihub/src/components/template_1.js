b33a811f8b2957cb872f6fa18d546426185a3c18
Danlei Mou <dmou@student.unimelb.edu.au>
2020-10-16 08:35:39 +0800

    Revert "done preview page"
    This reverts commit 8b0f37b28815e4d62a6198bf240d086d4ac73ef7.

--- folihub/src/components/template_1.js
+++ folihub/src/components/template_1.js
@@ -82,7 +82,7 @@ class Template_1 extends React.Component {
                 </SaveDiv>
                 <Container_1>
                     <IMG_1_Container>
-                        <IMG_1 src={this.state.base64} />
+                        <IMG_1 src={this.state.img} />
                         <Input type="file" name="image" onChange={this.onImageChange} />
                     </IMG_1_Container>
                     <TXT_1>
