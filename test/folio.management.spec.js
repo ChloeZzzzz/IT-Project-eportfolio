6913c804ed91f0f4eeb8bfb82658bf95eafff755
Yifei Zhu <yizhu1@student.unimelb.edu.au>
2020-11-17 21:25:13 +0800

    create eportfolio test

--- test/folio.management.spec.js
+++ test/folio.management.spec.js
@@ -9,6 +9,27 @@ const db = require('../config/database.js');
 
 var ePortfolioController = require('../controllers/ePortfolioController');
 
-var app = 'https://folihub-api.herokuapp.com/user';
+var app = 'https://folihub-api.herokuapp.com/ePortfolio';
 
 chai.use(chaiHttp);
+describe("POST / createEPortfolio", function (done) {
+    it ("should successfully send the request to the database", function (done) {
+        this.timeout(5000);
+        let data = {};
+        data.email = "test@test.test";
+        data.folioName = "test";
+        data.visibility = "public";
+        data.layout = "landscape";
+
+        chai.request(app)
+            .post('/createEPortfolio')
+            .set('content-type', 'application/x-www-form-urlencoded')
+            .send(data)
+            .end(function(err, response) {
+                expect(response).to.has.status(200);
+                done();
+            });
+    });
+})
+
+describe("POST / ")
