0132f58dc83c818201691b23175182d560d1f232
Yifei Zhu <40694262+ChloeZzzzz@users.noreply.github.com>
2020-11-19 00:40:16 +0800

    delete wrong pull request

--- controllers/ePortfolioController.js
+++ controllers/ePortfolioController.js
@@ -237,39 +237,6 @@ const getEportfolio = async (req, res) => {
   return;
 };
 
-const deleteLastPage = async (req, res) => {
-  console.log(req.body);
-  let { email, folioId } = req.body;
-  try {
-    console.log(email);
-    console.log(folioId);  
-    await db.query(`DELETE FROM Pages WHERE PageID = "30"`, (err, result) => {
-      if (err){
-        console.log("DELETE RESULT");
-        console.log(err)
-      }
-      console.log("DELETE RESULT");
-      console.log(result);
-    });
-
-    await db.query(`DELETE FROM Contents WHERE ContentID = "31"`, (err, result) => {
-      if (err){
-        console.log("DELETE RESULT");
-        console.log(err)
-      }
-      console.log("DELETE RESULT");
-      console.log(result);
-      res.status(200).send(result);
-    });
-  } catch (err) {
-    console.log("---get EP ERROR---");
-    console.log(err);
-    return res.end();
-  }
-
-  return;
-};
-
 const getPage = async(req, res) => {
   console.log(req.body);
   let { email, folioId, pageId } = req.body;
